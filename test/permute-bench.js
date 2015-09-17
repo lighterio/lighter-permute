28745ba06b1f5a82f48efc2ed55e2083bce90057
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:07 -0700

    Initial commit

---
+++ test/permute-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var permute = require(__dirname + '/lighter-permute')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/permute-test.js
@@ -0,0 +1,26 @@
+var permute = require('../lighter-permute')
+var is = global.is || require('exam/lib/is')
+
+describe('permute', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
